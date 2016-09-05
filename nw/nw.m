6b8ba85c3ff65cc7c6b768cc9c7907555982a6b1
mreiske <monique.reiske@hu-berlin.de>
2016-09-05 12:38:59 +0200

    code adjusted to styleguide

--- nw/nw.m
+++ nw/nw.m
@@ -1,7 +1,7 @@
 function sker=nw(x, vecX, vecY, h, K)   %Nadaraya-Watson Estimator
-vx = size(x);
-vY = size(vecY);
-vX = size(vecX);
-vh = size((vecX-x)/h');
+    vx   = size(x);
+    vY   = size(vecY);
+    vX   = size(vecX);
+    vh   = size((vecX-x)/h');
 
-sker = 1/h*kernel((vecX-x)./h,K)'*vecY;
+    sker = 1/h*kernel((vecX-x)./h,K)'*vecY;
--- regxest/regxest.m
+++ regxest/regxest.m
@@ -18,10 +18,10 @@
 %                recommended, which uses the faster WARPing method.
 % -----------------------------------------------------------------
 % Reference      Haerdle, W. (1990). Applied Nonparametric Regression.
-%		 Econometric Society Monographs, No. 19. Cambridge
-%		 University Press.
+%		         Econometric Society Monographs, No. 19. Cambridge
+%		         University Press.
 % -----------------------------------------------------------------
-% Usage        mh = regxest(x {,h {,K} {,v} })
+% Usage          mh = regxest(x {,h {,K} {,v} })
 %  Input
 %    Parameter  x  
 %    Definition   n x 2 matrix, the data. The first column contains the
@@ -46,44 +46,41 @@
 %                 on the values of the first column.
 % -----------------------------------------------------------------
 
-function[mh]=regxest(x,h,K)
-[n,m]=size(x);
-  if m ~= 2
-    disp('regxest: data matrix must contain 2 columns');
-    disp('Please input the data matrix again');
-    dat = input('x=');
-  end
-;
-eh = exist('h');
-  if (eh == 0)
-    %h=(max(x(:,1))-min(x(:,1)))/5;
-    h = 2.42*std(x(:,1))*n^(-0.2);
-  end
-eK = exist('K');
-  if (eK == 0)
-    K=1;        %quartic kernel
-  else
-      if K == 0 || K >4
-        disp('Type of the kernel must be "1", "2", "3" or "4"');
-        disp('The quartic kernel will be used');
-        K = 1;
-      end  
-  end
-;
-  tmp=sortrows(x)
-  x=tmp(:,1);
-  y=tmp(:,2);
-  %if (exist(v)==0)
-  %  v=x
-  % else
-  %  v=sort(v)
-  % endif
-;
-% require nw (Nadaraya-Watson) function / toolbox
+function[mh] = regxest(x,h,K)
+    [n,m] = size(x);
+    if m ~= 2
+        disp('regxest: data matrix must contain 2 columns');
+        disp('Please input the data matrix again');
+        dat = input('x=');
+    end
+    eh = exist('h');
+    if (eh == 0)
+        %h=(max(x(:,1))-min(x(:,1)))/5;
+        h = 2.42*std(x(:,1))*n^(-0.2);
+    end
+    eK = exist('K');
+    if (eK == 0)
+        K=1;        %quartic kernel
+    else
+        if K == 0 || K >4
+            disp('Type of the kernel must be "1", "2", "3" or "4"');
+            disp('The quartic kernel will be used');
+            K = 1;
+        end  
+    end
+    tmp = sortrows(x);
+    x   = tmp(:,1);
+    y   = tmp(:,2);
+    %if (exist(v)==0)
+        %v = x;
+    %else
+        %v = sort(v);
+    %endif
+    %require nw (Nadaraya-Watson) function / toolbox
     for i = 1:n  
         s(i,1)= nw(x(i),x,y,h,K)./nw(x(i),x,ones(n,1),h,K);
     end
     %for i = 1:n  % Gaussian kernel
-     %   s(i,1)= nw(x(i),x,y,5*h,K)./nw(x(i),x,ones(n,1),5*h,K)
+        %s(i,1)= nw(x(i),x,y,5*h,K)./nw(x(i),x,ones(n,1),5*h,K);
     %end
     mh=[x s];
