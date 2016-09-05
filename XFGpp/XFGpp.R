a1f72afd5220f764c54ac996d4be745bac5720d8
mreiske <monique.reiske@hu-berlin.de>
2016-09-05 11:52:35 +0200

    code adjusted to styleguide

--- XFGpp/XFGpp.R
+++ XFGpp/XFGpp.R
@@ -3,42 +3,36 @@ graphics.off()
 
 # p-p define function
 XFGpp = function(MMPL,PL){
-  PP = matrix(0,ncol(PL)+1,3)
-  true = matrix(0,nrow(MMPL),ncol(PL))
-  
-for (i in 1:nrow(MMPL)){
-  k = 1
-  while (k < (ncol(PL))){
-    true[i,k] = c(MMPL[i,]>PL[i,k]) # true = 1 for any value of MMPL>PL
- k = k + 1
- } 
-}
-
-tmp = apply(1-true,1,sum) # sum up all "true"s for each row
-
-#  tmp1 = tmp + (tmp<ncol(PL))
-
-# estimate pdf of each point
-  j=1
-  while (j < nrow(PL)) {
-  	    PP[(ncol(PL)-tmp[j]+2),2] = PP[(ncol(PL)-tmp[j]+2),2] + 1/nrow(PL) 
-    j = j + 1
-}
-  PP[1:50,]
-  PP[2:(ncol(PL)+1),1] = (1:ncol(PL))/ncol(PL)            # sequence
-  PP[2:(ncol(PL)+1),2] = cumsum(PP[2:(ncol(PL)+1),2])     # cum sum of empirical distribution of Loss
-  PP[2:(ncol(PL)+1),3] = (PP[2:(ncol(PL)+1),2]-PP[2:(ncol(PL)+1),1])^2/(ncol(PL)+1) # Mean Squared Deviation in each point
-  
-  return(PP)
+    PP    = matrix(0,ncol(PL)+1,3)
+    true  = matrix(0,nrow(MMPL),ncol(PL))
+    for (i in 1:nrow(MMPL)){
+        k = 1
+        while (k < (ncol(PL))){
+            true[i,k] = c(MMPL[i,]>PL[i,k]) # true = 1 for any value of MMPL>PL
+            k = k + 1
+        } 
+    }
+    tmp = apply(1-true,1,sum) # sum up all "true"s for each row
+    # tmp1 = tmp + (tmp<ncol(PL))
+    # estimate pdf of each point
+    j=1
+    while (j < nrow(PL)) {
+        PP[(ncol(PL)-tmp[j]+2),2] = PP[(ncol(PL)-tmp[j]+2),2] + 1/nrow(PL) 
+        j = j + 1
+    }
+    PP[1:50,]
+    PP[2:(ncol(PL)+1),1] = (1:ncol(PL))/ncol(PL)            # sequence
+    PP[2:(ncol(PL)+1),2] = cumsum(PP[2:(ncol(PL)+1),2])     # cum sum of empirical distribution of Loss
+    PP[2:(ncol(PL)+1),3] = (PP[2:(ncol(PL)+1),2]-PP[2:(ncol(PL)+1),1])^2/(ncol(PL)+1) # Mean Squared Deviation in each point
+    return(PP)
 }
 
 ############################
 ##    Basic Simulation    ##
 ############################
 
-
 # load data
-PL = read.table("XFGPL.dat")
+PL   = read.table("XFGPL.dat")
 MMPL = read.table("XFGMMPL.dat")
 
 # compute probability-probability plot
@@ -47,13 +41,12 @@ sum(PP[,3]) 	# Mean Squared Deviation
 
 par(mfrow=c(2,2))
 
-titl  = "Basic Simulation"
+titl   = "Basic Simulation"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
 
 # plot
-
 plot(PP[,1],PP[,1], type="l", col="blue3", lwd=3, ylab=ylabel, xlab=xlabel, main=titl, frame=T, axes=F)
 axis(1,c(0,0.5,1))
 axis(2,c(0,0.5,1))
@@ -64,14 +57,14 @@ lines(PP[,1],PP[,2],  col="red3", lwd=3)
 ###########################
 
 # load data
-PL = read.table("XFGPL2.dat")
+PL   = read.table("XFGPL2.dat")
 MMPL = read.table("XFGMMPL2.dat")
 
 # compute probability-probability plot
 PP = XFGpp(MMPL,PL)
 sum(PP[,3]) 	# Mean Squared Deviation
 
-titl  = "Mean Adjustment"
+titl   = "Mean Adjustment"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
@@ -81,21 +74,20 @@ plot(PP[,1],PP[,1], type="l", col="blue3", lwd=3, ylab=ylabel, xlab=xlabel, main
 axis(1,c(0,0.5,1))
 axis(2,c(0,0.5,1))
 lines(PP[,1],PP[,2],  col="red3", lwd=3)
-
 
 ###############################
 ##    Volatility Updating    ##
 ###############################
 
 # load data
-PL = read.table("XFGPL3.dat")
+PL   = read.table("XFGPL3.dat")
 MMPL = read.table("XFGMMPL3.dat")
 
 # compute probability-probability plot
 PP = XFGpp(MMPL,PL)
 sum(PP[,3]) 	# Mean Squared Deviation
 
-titl  = "Volatility Updating"
+titl   = "Volatility Updating"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
@@ -105,22 +97,20 @@ plot(PP[,1],PP[,1], type="l", col="blue3", lwd=3, ylab=ylabel, xlab=xlabel, main
 axis(1,c(0,0.5,1))
 axis(2,c(0,0.5,1))
 lines(PP[,1],PP[,2],  col="red3", lwd=3)
-
 
 #################################################
 ##    Volatility Updating & Mean Adjustment    ##
 #################################################
 
 # load data
-PL = read.table("XFGPL4.dat")
+PL   = read.table("XFGPL4.dat")
 MMPL = read.table("XFGMMPL4.dat")
 
 # compute probability-probability plot
-PP=XFGpp(MMPL,PL)
+PP = XFGpp(MMPL,PL)
 sum(PP[,3]) 	# Mean Squared Deviation
 
-
-titl  = "Volatility Updating & Mean Adjustment"
+titl   = "Volatility Updating & Mean Adjustment"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
@@ -131,15 +121,12 @@ axis(1,c(0,0.5,1))
 axis(2,c(0,0.5,1))
 lines(PP[,1],PP[,2],  col="red3", lwd=3)
 
-
-
 ###########################
 ##    Benchmark Curve    ##
 ###########################
 
-
 # load data
-PL = read.table("XFGPL5.dat")
+PL   = read.table("XFGPL5.dat")
 MMPL = read.table("XFGMMPL5.dat")
 
 # compute probability-probability plot
@@ -149,13 +136,12 @@ sum(PP[,3]) 	# Mean Squared Deviation
 dev.new()
 par(mfrow=c(2,2))
 
-titl  = "Benchmark Curve"
+titl   = "Benchmark Curve"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
 
 # plot
-
 plot(PP[,1],PP[,1], type="l", col="blue3", lwd=3, ylab=ylabel, xlab=xlabel, main=titl, frame=T, axes=F)
 axis(1,c(0,0.5,1))
 axis(2,c(0,0.5,1))
@@ -166,14 +152,14 @@ lines(PP[,1],PP[,2],  col="red3", lwd=3)
 ########################
 
 # load data
-PL = read.table("XFGPL6.dat")
+PL   = read.table("XFGPL6.dat")
 MMPL = read.table("XFGMMPL6.dat")
 
 # compute probability-probability plot
 PP = XFGpp(MMPL,PL)
 sum(PP[,3]) 	# Mean Squared Deviation
 
-titl  = "Spread Curve"
+titl   = "Spread Curve"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
@@ -183,21 +169,20 @@ plot(PP[,1],PP[,1], type="l", col="blue3", lwd=3, ylab=ylabel, xlab=xlabel, main
 axis(1,c(0,0.5,1))
 axis(2,c(0,0.5,1))
 lines(PP[,1],PP[,2],  col="red3", lwd=3)
-
 
 #################################
 ##    Conservative Approach    ##
 #################################
 
 # load data
-PL = read.table("XFGPL7.dat")
+PL   = read.table("XFGPL7.dat")
 MMPL = read.table("XFGMMPL7.dat")
 
 # compute probability-probability plot
 PP = XFGpp(MMPL,PL)
 sum(PP[,3]) 	# Mean Squared Deviation
 
-titl  = "Conservative Approach"
+titl   = "Conservative Approach"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
@@ -208,21 +193,19 @@ axis(1,c(0,0.5,1))
 axis(2,c(0,0.5,1))
 lines(PP[,1],PP[,2],  col="red3", lwd=3)
 
-
 ###################################
 ##    Simultaneous Simulation    ##
 ###################################
 
 # load data
-PL = read.table("XFGPL8.dat")
+PL   = read.table("XFGPL8.dat")
 MMPL = read.table("XFGMMPL8.dat")
 
 # compute probability-probability plot
 PP = XFGpp(MMPL,PL)
 sum(PP[,3]) 	# Mean Squared Deviation
 
-
-titl  = "Simultaneous Simulation"
+titl   = "Simultaneous Simulation"
 
 xlabel = "Uniform Distribution"
 ylabel = "Empirical Distribution"
