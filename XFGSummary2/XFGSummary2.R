a07e22ced46ba23e0ac6060a4139d5fd86441204
nazaretl <nazaretl@hu-berlin.de>
2016-09-14 12:14:33 +0200

    added from qualifying

--- XFGSummary2/XFGSummary2.R
+++ XFGSummary2/XFGSummary2.R
@@ -5,34 +5,33 @@ graphics.off()
 
 # set working directory to source file location and read the data
 # setwd("C:/...")
-x = read.table("Returns.txt", header = TRUE)
+x = read.table("XFGSummary2Returns", header = TRUE)
 
 
 # define the function
 sum.stat = function(X) {
 
-  # Computes the mean, standard deviation, median, quantiles (10% and 90 %),
-  # skewness and kurtosis of the input data. If the the input data is a
-  # matrix, the calculations are done on the columns. 
-  #
-  # Args:
-  #   x: Input Data
+    # Computes the mean, standard deviation, median, quantiles (10% and 90 %),
+    # skewness and kurtosis of the input data. If the the input data is a
+    # matrix, the calculations are done on the columns. 
+    #
+    # Args:
+    #   x: Input Data
 
-  m   = apply(x,2,mean)  # mean
-  sd  = apply(x, 2, sd)  # standard deviation
-  med = apply(x, 2, median)  # median
-  q   = apply(x, 2, quantile, probs = c(0.1, 0.9))  # quantiles
+    m    = apply(x,2,mean)  # mean
+    sd   = apply(x, 2, sd)  # standard deviation
+    med  = apply(x, 2, median)  # median
+    q    = apply(x, 2, quantile, probs = c(0.1, 0.9))  # quantiles
 
-  z    = scale(x)  # normalize columns of x
-  skew = colSums(z^3)/(dim(x)[1])  # skewness
-  kurt = colSums(z^4)/(dim(x)[1])  # kurtosis
+    z    = scale(x)  # normalize columns of x
+    skew = colSums(z^3)/(dim(x)[1])  # skewness
+    kurt = colSums(z^4)/(dim(x)[1])  # kurtosis
 
-  tab            = data.frame(cbind(m, sd, med, t(q), skew, kurt))  # create table with results
-  names(tab)     = c("Mean", "SD", "Median", "0.1-q", "0.9-q", "Skewness",
-                     "Kurtosis")  # name table columns
-  row.names(tab) = names(x)  # name table rows
+    tab            = data.frame(cbind(m, sd, med, t(q), skew, kurt))  # create table with results
+    names(tab)     = c("Mean", "SD", "Median", "0.1-q", "0.9-q", "Skewness","Kurtosis")  # name table columns
+    row.names(tab) = names(x)  # name table rows
 
-  return(tab)
+    return(tab)
 }
 
 # call function
