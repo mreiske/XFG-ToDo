7949226aa6c0063fee987e4a63ea823613806a23
Monique Reiske <monique.reiske@hu-berlin.de>
2016-09-14 14:38:56 +0200

    Metainfo adjustments (#2)
    _Qualifying: correct codes and metainfo
    __Valid Code and Metainfo - Missing Information: correct codes, valid YAML, missing info e.g. keywords

---
+++ XFGIBT02_doesnt run/XFGIBT02 (1).m
@@ -0,0 +1,53 @@
+% ---------------------------------------------------------------------
+% Book:         XFG
+% ---------------------------------------------------------------------
+% Quantlet:     XFGIBT02
+% ---------------------------------------------------------------------
+% Description:  plots an estimated state price density of the stock prices
+%               estimated by implied binomial tree and plots the estimated
+%               implied local volatility surface
+% ---------------------------------------------------------------------
+% Usage:        IBTbc, IBTdk, IBTimpliedvola, IBTblackscholes, IBTcrr,
+%               IBTlocsigma, IBTsdisplot, IBTvolaplot, regxest,
+%----------------------------------------------------------------------
+% Inputs:       Price of underlying asset, Interest rate, Time to
+%               expiration, Number of steps
+% ---------------------------------------------------------------------
+% Output:       Plot of the estimated state price density and the estimated
+%               implied local volatility surface
+% ---------------------------------------------------------------------
+% Example:     
+% ---------------------------------------------------------------------
+% Author:      Wolfgang Haerdle, Jun Zheng, Alena Mysickova 
+% ---------------------------------------------------------------------
+format short
+disp('Please input Price of Underlying Asset s0, Riskless Interest Rate per Year r');
+disp('Time to Expiration (Years) t, Number of steps n');
+disp('as: [100, 0.03, 5, 40]');
+disp(' ') ;
+para=input('[s0, r, t, n]=');
+while length(para) < 4
+  disp('Not enough input arguments. Please input in 1*4 vector form like [100, 0.03, 5, 5]');
+  disp(' ') ;
+  para=input('[s0, r, t, n]=');
+end
+s0=para(1);             % Stock price
+r=para(2);              % Riskless interest rate	
+t=para(3);			    % Time to expiration
+n=para(4);              % Number of intervals format short
+[St, AD, P, LV] = XFGIBTcdk(s0,r,t,n,[]);          % Derman & Kani
+%[St, AD, P, LV] = IBTbc(s0,r,t,n,[]);         % Barle & Cakici    
+dat=[St(:,n+1) (AD(:,n+1)*exp(r*t))];
+bandwidth=20;
+in = find(dat(:,2)>0.005);
+dat = dat(in,:);
+figure(1)
+IBTsdisplot(dat, bandwidth); % Density plot
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+deltat=t/n;
+[loc,lv,strike,mat]=IBTlocsigma(St, P, n, deltat);
+startpoint=0
+endpoint=150
+figure(2)
+IBTvolaplot(loc,deltat,startpoint,endpoint,20); % Implied local volatility surface
+
